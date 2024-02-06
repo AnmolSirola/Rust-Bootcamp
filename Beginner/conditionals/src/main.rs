6e35294faa11179d9a98c8e2118cfe55cdfdfc86
Anmol <anmolsirola57@gmail.com>
2024-02-06 13:11:38 +0530

    Conditionals

---
+++ Beginner/conditionals/src/main.rs
@@ -0,0 +1,16 @@
+//control flow
+
+fn main(){
+
+    let food = "cookie"
+
+    if food == "cookie" {
+        println!("I like cookies too!");
+
+    } else if food == "fruit"{
+        println!("That sounds healthy!");
+
+    } else{
+        println!("Oh, that's bad!");
+    }
+}
