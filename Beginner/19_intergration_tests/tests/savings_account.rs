0bf3b6272205f39aff5eb21d41bc59d9e6093dfa
Anmol <anmolsirola57@gmail.com>
2024-04-15 21:26:06 +0530

    Integration test and generics

---
+++ Beginner/19_intergration_tests/tests/savings_account.rs
@@ -0,0 +1,11 @@
+
+use bank::SavingsAccount;
+
+#[test]
+
+fn should_have_a_starting_balance_of_0() {
+    utils::common_setup();
+    let account = SavingsAccount::new() {
+        assert_eq!(account.get_balance(), 0)
+    }
+}
